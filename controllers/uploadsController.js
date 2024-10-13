471c9406abefd06a7d90002b0791926bf2c66a6e
TaHa <madax361@gmail.com>
2024-10-13 14:25:11 +0300

    uploaded images to Cloudinary

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,9 +1,28 @@
 const path = require('path')
+const CustomError = require('../errors')
 
 const { StatusCodes } = require('http-status-codes')
+const cloudinary = require('cloudinary').v2
+const fs = require('fs')
 
-const uploadProductImage = async (req, res) => {
-  let productImage = req.files.imageComputer
+const uploadProductImageLocal = async (req, res) => {
+  if (!req.files) {
+    throw new CustomError.BadRequestError('No File Uploaded')
+  }
+  const productImage = req.files.image
+  console.log(productImage)
+
+  if (!productImage.mimetype.startsWith('image')) {
+    throw new CustomError.BadRequestError('Please Upload Image')
+  }
+
+  const maxSize = 1000
+
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError(
+      'Please Upload an image that is less than 1KB'
+    )
+  }
   const imagePath = path.join(
     __dirname,
     `../public/uploads/${productImage.name}`
@@ -14,6 +33,18 @@ const uploadProductImage = async (req, res) => {
   res
     .status(StatusCodes.OK)
     .json({ image: { src: `/uploads/${productImage.name}` } })
+}
+
+const uploadProductImage = async (req, res) => {
+  const result = await cloudinary.uploader.upload(
+    req.files.image.tempFilePath,
+    {
+      use_filename: true,
+      folder: 'file-upload',
+    }
+  )
+  fs.unlinkSync(req.files.image.tempFilePath)
+  return res.status(StatusCodes.OK).json({ image: { src: result.secure_url } })
 }
 
 module.exports = {
